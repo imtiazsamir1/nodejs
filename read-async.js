545d8f9679e70dc137225b3f10f7545c03a76f84
imtiazsamir1 <imtiazsamir416@gmail.com>
2025-11-29 12:18:51 +0600

    read-async

---
+++ read-async.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+console.log("start reading...");
+fs.readFile("./data/diary.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.error("error happend :", error.message);
+  }
+  console.log("file contant:");
+  console.log(data);
+});
+console.log("This runs immediatly-no blocking");
---
+++ write-file.js
@@ -0,0 +1,3 @@
+const fs = require("fs");
+
+const content1 = "This is a content \n nodejs is awesome!!!";
